93aa0544be7819cc4326346d19fd28a953f7850b
Longoose <longoose@klonopin.(none)>
2013-07-26 12:00:55 -0400

    commit numero uno

---
+++ pyspamsms.py
@@ -0,0 +1,20 @@
+import smtplib
+import string
+ 
+SUBJECT = "INCOMING COMM"
+TO = "********@messaging.sprintpcs.com"
+FROM = "instantiate@whatever.org"
+i = 0
+while i < 5:
+	FROM = "STCFOREVER" + str(i) + "@spamtec.org"
+	text = "ALL HAIL THE SPAM GOD."
+	BODY = string.join((
+        	"From: %s" % FROM,
+        	"To: %s" % TO,
+        	"Subject: %s" % SUBJECT ,
+        	"",
+        	text
+        	), "\r\n")
+	server = smtplib.SMTP(HOST)
+	server.sendmail(FROM, [TO], BODY)
+	server.quit()
