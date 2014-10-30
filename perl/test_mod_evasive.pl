a5d5f8927272a3ee4a74d9af550c25d8bd3c7163
dannykansas <dan@logickc.com>
2014-10-30 10:21:26 -0400

    adding mod_evasive tester

---
+++ perl/test_mod_evasive.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+ 
+# test_mod_evasive.pl: small script to test mod_dosevasive's effectiveness
+ 
+use IO::Socket;
+use strict;
+ 
+for(0..100) {
+  my($response);
+  my($SOCKET) = new IO::Socket::INET( Proto   => "tcp",
+                                      PeerAddr=> "target.com:80");
+  if (! defined $SOCKET) { die $!; }
+  print $SOCKET "GET /?$_ HTTP/1.0nn";
+  $response = <$SOCKET>;
+  print $response;
+  close($SOCKET);
+}
